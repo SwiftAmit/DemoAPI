e3e917e0da50aec06b4eb34e84aa43ae872a4181
amit <amit400gupta@gmail.com>
2017-12-18 00:24:41 +0530

    network wrapper

---
+++ DemoAPI/Source/Model/DAHomeModel.swift
@@ -0,0 +1,9 @@
+//
+//  DAHomeModel.swift
+//  DemoAPI
+//
+//  Created by Amit on 17/12/2015 SAKA.
+//  Copyright © 2017 saffron. All rights reserved.
+//
+
+import Foundation
---
+++ DemoAPI/Utility/DASwiftLoader.swift
@@ -0,0 +1,9 @@
+//
+//  DASwiftLoader.swift
+//  DemoAPI
+//
+//  Created by Amit on 17/12/2015 SAKA.
+//  Copyright © 2017 saffron. All rights reserved.
+//
+
+import Foundation
